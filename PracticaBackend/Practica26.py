4de7af4008ae678ebe8a3391ecbec633feee0658
ivanpfoh <ivanpfohh85@gmail.com>
2024-08-11 03:44:35 -0300

    msg: DBpractice

--- PracticaBackend/Practica26.py
+++ PracticaBackend/Practica26.py
@@ -1,10 +1,11 @@
 import sqlite3
 
-conexion = sqlite3.connect('ejemplo.db', 'r')
+conexion = sqlite3.connect('ejemplo.db')
 
 cursor = conexion.cursor()
-
-
-
+nombre = input("Ingrese el nombre que quiere consultar en la base de datos:\n> ")
+cursor.execute("SELECT * FROM usuarios WHERE nombre = '{}'".format(nombre))
+usuarios = cursor.fetchall()
+print(usuarios)
 conexion.commit()
 conexion.close()
